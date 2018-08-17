4c297206aab12fab6514f1af1357d24fee66ddc3
Darwin Hang <darwin.hang@target.com>
2018-08-17 11:09:08 -0500

    add package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='coco_text', version='1.1', description='toolbox for COCO-text')
+setup(name='coco_text', version='1.1', description='toolbox for COCO-text', packages=['coco_text'])
